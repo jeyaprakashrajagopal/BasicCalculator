85e6d10efafd8ad87a635e457e445c1eca9d200c
Jeyaprakash <jeyaprakash.rajagopal@gmail.com>
2024-01-01 06:55:37 +0100

    removing the unnecessary derivedStateOf usage

--- app/src/main/java/com/renraj/basiccalculator/BasicCalculatorNavigation.kt
+++ app/src/main/java/com/renraj/basiccalculator/BasicCalculatorNavigation.kt
@@ -2,8 +2,6 @@ package com.renraj.basiccalculator
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -22,13 +20,9 @@ fun ColorfulCalculatorNavigation() {
         composable(BasicCalculatorScreens.Home.route) {
             val calculatorViewModel = hiltViewModel<BasicCalculatorViewModel>()
             val calculatorState = calculatorViewModel.result.collectAsState().value
-            val derivedResult by remember(calculatorState) {
-                derivedStateOf {
-                    (calculatorState.firstNumber ?: "") +
-                            (calculatorState.operator?.symbol ?: "") +
-                            (calculatorState.secondNumber ?: "")
-                }
-            }
+            val derivedResult = (calculatorState.firstNumber ?: "") +
+                    (calculatorState.operator?.symbol ?: "") +
+                    (calculatorState.secondNumber ?: "")
             val onCalculatorAction = remember {
                 return@remember calculatorViewModel::onCalculatorAction
             }
--- app/src/main/java/com/renraj/basiccalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/renraj/basiccalculator/ui/theme/Theme.kt
@@ -3,10 +3,10 @@ package com.renraj.basiccalculator.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
